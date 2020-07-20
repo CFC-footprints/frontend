68035a9ee5bda76dd71d47a29474564bcfee280b
Nicolas Romero Toledo <nromerot@ripley.com>
2020-07-19 23:46:09 -0400

    First approach to OSM using leaflet lib

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,16 +1,43 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
+import Image from "react-bootstrap/Image";
+import { FaUserAlt } from "react-icons/fa";
+import { useTypedSelector } from "../../store/reducers";
 
-import "./styles.css";
+import "./styles.scss";
+import logo from "../../images/Logo.png";
 
 function Header() {
+  const tokens = useTypedSelector((state) => state.tokens);
+  let userName = null;
+
+  if (tokens) {
+    const decodeIDToken = tokens.idTokenPayload;
+    userName = decodeIDToken.name;
+  }
+
   return (
     <Container fluid>
       <div className="header-container">
         <div className="header">
           <Navbar>
-            <Navbar.Brand href="#">CFC - Water Footprint</Navbar.Brand>
+            <Navbar.Brand href="#">
+              <img
+                src={logo}
+                width="50"
+                height="50"
+                className="d-inline-block"
+                alt="React Bootstrap logo"
+              />{" "}
+              Water Footprint
+            </Navbar.Brand>
+            <Navbar.Collapse className="justify-content-end">
+              {userName ? <FaUserAlt /> : null}
+              <div className="username">
+                <Navbar.Text>{userName}</Navbar.Text>
+              </div>
+            </Navbar.Collapse>
           </Navbar>
         </div>
       </div>
--- src/components/wizard/step3.tsx
+++ src/components/wizard/step3.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import DatePicker from "react-datepicker";
 import { InputGroup, FormControl } from "react-bootstrap";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import ActionButtons from "./actionButtons";
 import updateFootprint from "../../store/actions";
 import { useTypedSelector } from "../../store/reducers";
-
 import "react-datepicker/dist/react-datepicker.css";
+
+import "leaflet/dist/leaflet.css";
 
 function Step3(props: any) {
   const dispatch = useDispatch();
@@ -31,10 +33,29 @@ function Step3(props: any) {
     buttons = null;
   }
 
+  const position: [number, number] = [51.505, -0.09];
+  const map = (
+    <Map center={position} zoom={13}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup.
+          <br />
+          Easily customizable.
+        </Popup>
+      </Marker>
+    </Map>
+  );
+
   return (
     <div>
       <div className="wizard-step">
-        <h3>Latitud</h3>
+        <div className="wizard-maps">{map}</div>
+
+        {/* <h3>Latitud</h3>
         <InputGroup size="lg">
           <InputGroup.Prepend>
             <InputGroup.Text id="inputGroup-sizing-lg">Latitud</InputGroup.Text>
@@ -57,7 +78,7 @@ function Step3(props: any) {
             aria-describedby="inputGroup-sizing-sm"
             onChange={(e: any) => handleChange(e.target.value)}
           />
-        </InputGroup>
+        </InputGroup> */}
       </div>
       {buttons}
     </div>
