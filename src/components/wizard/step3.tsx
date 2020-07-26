be0ceef96ed4d8ed52a1ca584022ba5d30d0f03e
Nicolas Romero Toledo <nromerot@ripley.com>
2020-07-25 23:21:23 -0400

    refactor map component

--- src/components/wizard/step3.tsx
+++ src/components/wizard/step3.tsx
@@ -1,11 +1,9 @@
-/* eslint-disable global-require */
-import React, { createRef, useState } from "react";
+import React from "react";
 import { useDispatch } from "react-redux";
-import { InputGroup, FormControl } from "react-bootstrap";
-import { Map, Marker, TileLayer } from "react-leaflet";
-import L from "leaflet";
+import { InputGroup, FormControl, Button } from "react-bootstrap";
 import ActionButtons from "./actionButtons";
 import updateFootprint from "../../store/actions";
+import Map from "../map";
 import { useTypedSelector } from "../../store/reducers";
 
 import "react-datepicker/dist/react-datepicker.css";
@@ -14,17 +12,8 @@ import "leaflet/dist/leaflet.css";
 function Step3(props: any) {
   const dispatch = useDispatch();
   const initState = useTypedSelector((state) => state);
-
   const { nextStep, previousStep, showButtons = true } = props;
-
-  const refmarker = createRef<Marker>();
-
-  const { markers } = initState;
-
-  // own component state
-  const [lat, setLat] = useState("-33.47269");
-  const [lng, setLng] = useState("-70.64724");
-
+  const { lat, lng } = initState;
   let buttons;
 
   if (showButtons) {
@@ -33,74 +22,67 @@ function Step3(props: any) {
     buttons = null;
   }
 
-  function updatePosition() {
-    const marker = refmarker.current;
-    if (marker != null) {
-      const newMarkers = marker.leafletElement.getLatLng();
-      dispatch(updateFootprint({ markers: newMarkers }));
-      setLat(newMarkers.lat.toString());
-      setLng(newMarkers.lng.toString());
-    }
-  }
-
   // default position Chile - Santiago
   const position: [number, number] = [-33.47269, -70.64724];
 
-  const icon = L.icon({
-    iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-    iconUrl: require("leaflet/dist/images/marker-icon.png"),
-    shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
-    iconSize: [30, 45],
-  });
-
-  const map = (
-    <Map center={markers ? [markers.lat, markers.lng] : position} zoom={13}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <Marker
-        icon={icon}
-        draggable
-        ondragend={updatePosition}
-        position={markers ? [markers.lat, markers.lng] : position}
-        ref={refmarker}
-      />
-    </Map>
-  );
+  function handleSubmit() {
+    dispatch(
+      updateFootprint({
+        markers: {
+          lat: lat ? parseFloat(lat) : position[0],
+          lng: lng ? parseFloat(lng) : position[1],
+        },
+      })
+    );
+  }
 
   return (
     <div>
       <div className="wizard-step">
-        <div className="wizard-maps">{map}</div>
-        <InputGroup>
-          <InputGroup.Prepend>
-            <InputGroup.Text id="inputGroup-sizing-lg">Latitud</InputGroup.Text>
-          </InputGroup.Prepend>
-          <FormControl
-            aria-label="Default"
-            aria-describedby="inputGroup-sizing-sm"
-            value={lat}
-            onChange={(e) => {
-              setLat(e.target.value);
-            }}
-          />
-        </InputGroup>
-        <InputGroup>
-          <InputGroup.Prepend>
-            <InputGroup.Text id="inputGroup-sizing-lg">
-              Longitud
-            </InputGroup.Text>
-          </InputGroup.Prepend>
-          <FormControl
-            aria-label="Default"
-            aria-describedby="inputGroup-sizing-sm"
-            value={lng}
-            onChange={(e) => {
-              setLng(e.target.value);
-            }}
-          />
-        </InputGroup>
+        <div className="wizard-maps">
+          <Map />
+        </div>
+        <div className="wizard-coordinates">
+          <InputGroup>
+            <InputGroup.Prepend>
+              <InputGroup.Text id="inputGroup-sizing-lg">
+                Latitude
+              </InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl
+              aria-label="Default"
+              aria-describedby="inputGroup-sizing-sm"
+              value={lat || undefined}
+              onChange={(e) => {
+                dispatch(
+                  updateFootprint({
+                    lat: e.target.value,
+                  })
+                );
+              }}
+            />
+          </InputGroup>
+          <InputGroup>
+            <InputGroup.Prepend>
+              <InputGroup.Text id="inputGroup-sizing-lg">
+                Longitude
+              </InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl
+              aria-label="Default"
+              aria-describedby="inputGroup-sizing-sm"
+              value={lng || undefined}
+              onChange={(e) => {
+                dispatch(
+                  updateFootprint({
+                    lng: e.target.value,
+                  })
+                );
+              }}
+            />
+          </InputGroup>
+          <Button onClick={handleSubmit}>Get Address</Button>
+        </div>
       </div>
       {buttons}
     </div>
